d8d068501f694115cbf3a49933b95e59976df4db
Selormey Hubert <selormeyh@gmail.com>
2024-09-10 04:51:35 +0000

    Updated urls.py to include customers.urls, added customers/ path, and modified customers_List view in views.py to return JsonResponse with safe=False

--- customer/customers/views.py
+++ customer/customers/views.py
@@ -3,8 +3,8 @@ from .serializers import CustomerSerializer
 from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
 
-def customer_List(request):
+def customers_List(request):
     if request.method == 'GET':
         customers = Customer.objects.all()
         serializer = CustomerSerializer(customers, many=True)
-        return JsonResponse(serializer.data)
+        return JsonResponse(serializer.data, safe=False)
