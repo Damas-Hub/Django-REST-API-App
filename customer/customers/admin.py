57f7f60ffb9f44e1be6c0c528e64f8c6018183d2
Selormey Hubert <selormeyh@gmail.com>
2024-09-10 04:06:57 +0000

    Updated admin.py to register Customer model with custom admin class, and modified db.sqlite3 and admin.cpython-312.pyc files.

--- customer/customers/admin.py
+++ customer/customers/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-
 from .models import Customer
 
-admin.site.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['name','age','phone_number']
+
+admin.site.register(Customer, CustomerAdmin)
