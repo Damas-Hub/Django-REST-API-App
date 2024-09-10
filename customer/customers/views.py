08ad05318f1ff56808c14e935193f02d40a73c14
Selormey Hubert <selormeyh@gmail.com>
2024-09-10 05:34:05 +0000

    Updated views.py to add csrf_exempt decorator and CRUD functionality for customer details, and modified db.sqlite3

--- customer/customers/views.py
+++ customer/customers/views.py
@@ -20,7 +20,7 @@ def customers_List(request):
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
     
-    
+@csrf_exempt    
 def customer_details(request, pk):
     try:
         customer = Customer.objects.get(pk=pk)
@@ -30,3 +30,14 @@ def customer_details(request, pk):
     if request.method == 'GET':
         serializer = CustomerSerializer(customer)
         return JsonResponse(serializer.data)
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = CustomerSerializer(customer, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    if request.method == 'DELETE':
+         customer.delete()
+         return HttpResponse(status=204)
+        
