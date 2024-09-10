295f95cea8f3c169700f86d777d6009b3f387548
Selormey Hubert <selormeyh@gmail.com>
2024-09-10 04:58:12 +0000

    Updated customer app: added customer details view, modified urls and views files, and changed database

--- customer/customers/urls.py
+++ customer/customers/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.customers_List),
+    path('<int:pk>', views.customer_details)
+
 ]
--- customer/customers/views.py
+++ customer/customers/views.py
@@ -2,9 +2,31 @@ from .models import Customer
 from .serializers import CustomerSerializer
 from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
 
+
+@csrf_exempt
 def customers_List(request):
     if request.method == 'GET':
         customers = Customer.objects.all()
         serializer = CustomerSerializer(customers, many=True)
         return JsonResponse(serializer.data, safe=False)
+    
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = CustomerSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+    
+    
+def customer_details(request, pk):
+    try:
+        customer = Customer.objects.get(pk=pk)
+    except Customer.DoesNotExist:
+        return HttpResponse(status=404)
+    
+    if request.method == 'GET':
+        serializer = CustomerSerializer(customer)
+        return JsonResponse(serializer.data)
