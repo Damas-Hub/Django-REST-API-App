29929410206e2e419ff65855dd8a2e27c41441d2
Selormey Hubert <selormeyh@gmail.com>
2024-09-10 04:28:24 +0000

    Added rest_framework to INSTALLED_APPS, created customer_List view in customers/views.py, and updated settings.py and views.py files

--- customer/customers/views.py
+++ customer/customers/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from .models import Customer
+from .serializers import CustomerSerializer
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+def customer_List(request):
+    if request.method == 'GET':
+        customers = Customer.objects.all()
+        serializer = CustomerSerializer(customers, many=True)
+        return JsonResponse(serializer.data)
