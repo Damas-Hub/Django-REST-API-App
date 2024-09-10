19c0667da0ae11bd9a214a4ccd2778564d1ba9be
Selormey Hubert <selormeyh@gmail.com>
2024-09-10 12:40:43 +0000

    Removed unused imports and updated CustomerList class in views.py

--- customer/customers/views.py
+++ customer/customers/views.py
@@ -1,10 +1,5 @@
 from .models import Customer
 from .serializers import CustomerSerializer
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.views import APIView
-from django.http import Http404
 from rest_framework import mixins, generics
 
 class CustomerList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
