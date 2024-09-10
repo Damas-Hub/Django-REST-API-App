3d6196a3721b15eacf02ba029c4daafc0b028970
Selormey Hubert <selormeyh@gmail.com>
2024-09-10 11:19:55 +0000

    Refactor customers_List view from function-based to class-based APIView

--- customer/customers/views.py
+++ customer/customers/views.py
@@ -3,22 +3,36 @@ from .serializers import CustomerSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
  
-
-@api_view(['GET','POST'])
-def customers_List(request):
-    if request.method == 'GET':
+class CustomersList(APIView):
+    def get(self, request):
         customers = Customer.objects.all()
         serializer = CustomerSerializer(customers, many=True)
         return Response(serializer.data)
     
-    if request.method == 'POST':
-         
-        serializer = CustomerSerializer(data=request.data)
-        if serializer.is_valid():
+    def post(self,request):
+         serializer = CustomerSerializer(data=request.data)
+         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+#@api_view(['GET','POST'])
+#def customers_List(request):
+    #if request.method == 'GET':
+        #customers = Customer.objects.all()
+        #serializer = CustomerSerializer(customers, many=True)
+        #return Response(serializer.data)
+    
+    #if request.method == 'POST':
+         
+        #serializer = CustomerSerializer(data=request.data)
+        #if serializer.is_valid():
+            #serializer.save()
+            #return Response(serializer.data, status=status.HTTP_201_CREATED)
+        #return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET','POST','DELETE'])   
 def customer_details(request, pk):
